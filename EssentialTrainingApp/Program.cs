74b941017001cfbdf1b7ab3d4367ecad1277b716
JHarrell1211 <julian1211@gmail.com>
2018-05-03 15:55:20 -0400

    training complete

---
+++ EssentialTrainingApp/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace EssentialTrainingApp
+{
+    class Program
+    {
+        public static List<string> Words;
+        static void Main(string[] args)
+        {
+            Words = new List<string>();
+            Words.Add("Bread");
+            Words.Add("Milk");
+            Words.Add("Cheese");
+
+            CrazyMathProb();
+        }
+
+     
+
+        //Debug practice
+        private static int CrazyMathProb()
+        {
+            var income = 1000;
+
+            for (var i = 10; i > 0; i--)
+            {
+                income = income = (income / i);
+            }
+            return income;
+        }
+    }
+}
