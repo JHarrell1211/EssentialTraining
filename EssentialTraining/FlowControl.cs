63694f73f58606f57c4f21257b12e94ee09b9551
JHarrell1211 <julian1211@gmail.com>
2018-05-02 22:43:09 -0400

    first commit

---
+++ EssentialTraining/FlowControl.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EssentialTraining
+{
+    public class FlowControl
+    {
+        public string PrimaryOrSecondaryCompound(string color)
+        {
+            var result = "";
+            if(color.ToLower() == "red" || color.ToLower() == "blue" || color.ToLower() == "yellow")
+            {
+                result = "Primary";
+            }
+            else
+            {
+                result = "Secondary";
+            }
+            return result;
+        }
+        public string SecondaryOrPrimary(string color)
+        {
+            var result = "";
+
+            switch (color.ToLower())
+            {
+                case "red":
+                    result = "Primary";
+                    break;
+
+                case "blue":
+                    result = "Primary";
+                    break;
+
+                default:
+                    result = "Secondary";
+                    break;
+            }
+            return result;
+        }
+
+        public string PrimaryOrSecondary(string color)
+        {
+            var result = "";
+
+            if(color.ToLower() == "red")
+            {
+                result = "Primary";
+            }
+            else if(color.ToLower() == "blue")
+            {
+                result = "Primary";
+            }
+            else if(color.ToLower() == "yellow")
+            {
+                result = "Primary";
+            }
+            else
+            {
+                result = "Secondary";
+            }
+            return result;
+        }
+
+        public bool IsYourFavoriteColorYellow(string color)
+        {
+            return (color.ToLower() == "yellow");
+        }
+
+        public bool IsYourFavoriteColorRed(string color)
+        {
+            if (color.ToLower() == "red") return true;
+            return false;
+        }
+
+        public bool IsYourFavoritColorGreen(string color)
+        {
+            return (color.ToLower() == "green") ? true : false;
+        }
+
+        public bool IsYourFavoriteColorBlue(string color)
+        {
+            if(color.ToLower() == "blue")
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ EssentialTrainingTests/AwesomeSauceTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using EssentialTraining;
+
+namespace EssentialTrainingTests
+{
+    [TestClass]
+    public class AwesomeSauceTest
+    {
+        [TestMethod]
+        public void IsSauceAwesomeTest()
+        {
+            var testInstance = new AwesomeSauce();
+            testInstance.Sauces.Add("Tobasco");
+            testInstance.Sauces.Add("Texas Pete");
+            testInstance.Sauces.Add("B & M BBQ");
+
+            //expect true
+            Assert.IsTrue(testInstance.IsSauceAwesome("Texas Pete"));
+
+            //expect false
+            Assert.IsFalse(testInstance.IsSauceAwesome("Red Hot"));
+        }
+    }
+}
